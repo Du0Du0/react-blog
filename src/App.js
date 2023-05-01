3d599f69bae5b3033a001c867bc15ea02df09769
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 <dohyun911@hanmail.net>
2023-05-01 16:28:43 +0900

    feature: list state(2)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {useState} from 'react';
 function App() {
 
   let post = '리액트 스터디';
-  let [a,b] = useState('리액트는 왜 쓸까?');
+  let [listTitle,setlistTitle] = useState('리액트는 왜 쓸까?');
+  
 
   return (
     <div className="App">
@@ -13,7 +14,7 @@ function App() {
       <h4 style={ {color : '#e9e9e9', fontSize : '16px'} }>Du0Du0 developer blog</h4>
       </div>
       <div className="list">
-     <h4>{a}</h4>
+     <h4>{listTitle}</h4>
      <p>2023-05-01</p>
       </div>
     </div>
