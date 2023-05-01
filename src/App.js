c5cf6c17a73dee1ecf797d41bcf3021053041502
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 <dohyun911@hanmail.net>
2023-05-01 17:09:16 +0900

    feature: like-Btn 누르면 1증가하는 기능추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
 function App() {
 
   let post = '리액트 스터디';
-  let [listTitle,setlistTitle] = useState('리액트는 왜 쓸까?');
-  
+  let [listTitle,setlistTitle] = useState(['리액트는 왜 쓸까?','jsx는 무엇일까?', 'state는 언제쓸까?']);
+  let [likeBtn, setlikeBtn] = useState(0);
+
 
   return (
     <div className="App">
@@ -14,7 +14,15 @@ function App() {
       <h4 style={ {color : '#e9e9e9', fontSize : '16px'} }>Du0Du0 developer blog</h4>
       </div>
       <div className="list">
-     <h4>{listTitle}</h4>
+     <h4>{listTitle[0]}<span onClick={() => {setlikeBtn(likeBtn+1)}}>👍</span>{likeBtn}</h4>
+     <p>2023-05-01</p>
+      </div>
+      <div className="list">
+     <h4>{listTitle[1]}</h4>
+     <p>2023-05-01</p>
+      </div>
+      <div className="list">
+     <h4>{listTitle[2]}</h4>
      <p>2023-05-01</p>
       </div>
     </div>
