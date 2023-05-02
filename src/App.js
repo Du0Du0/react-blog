820815e5d07dc118e7f2ba56765f3522cab7a726
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 <dohyun911@hanmail.net>
2023-05-02 17:34:15 +0900

    fix: 정렬 select 버튼 컴포넌트 만들기 및 모달 열기닫기 기능 추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,64 @@ function Modal(props) {
   )
 }
 
+function ArrayBtn(props) {
+  const {listTitle, likeBtn, setlistTitle, setlikeBtn} = props;
+  return (
+    <>
+      {/* 정렬종류 select 버튼 */}
+         <div className='array-select-btn'>
+   
+         {/* 오름차순으로 정렬하는 버튼 */}
+         <div onClick={() => {
+          const data =  props.listTitle.map((a, i) => {
+             return {'listTitle' : props.listTitle[i], 'likeBtn':  props.likeBtn[i] }
+           });
+         
+           let ascArr = data.sort((a,b)=> a.listTitle.localeCompare(b.listTitle));
+           let sortedTitle =  ascArr.map(item => item.listTitle);
+           let sortedLike =  ascArr.map(item => item.likeBtn);
+           setlistTitle(sortedTitle);
+           setlikeBtn(sortedLike);
+         }}>
+         <p>오름차순</p></div>
+         
+         
+         {/* 내림차순으로 정렬하는 버튼 */}
+         <div onClick={()=> {
+         const data =  props.listTitle.map((a, i) => {
+           return {'listTitle' : props.listTitle[i], 'likeBtn':  props.likeBtn[i] }
+         });
+   
+         let descArr = data.sort((a,b)=> a.listTitle.localeCompare(b.listTitle)).reverse();
+         let sortedTitle =  descArr.map(item => item.listTitle);
+         let sortedLike =  descArr.map(item => item.likeBtn);
+         setlistTitle(sortedTitle);
+         setlikeBtn(sortedLike);
+         }}><p>내림차순</p></div>
+   
+         {/* 좋아요순으로 정렬하는 버튼 */}
+         <div onClick={()=> {
+         
+         const data = props.listTitle.map((a , i) => {
+           return { 'listTitle':   props.listTitle[i], 'likeBtn':  props.likeBtn[i] };
+         });
+   
+           let likeArr = data.sort((a,b)=> b.likeBtn- a.likeBtn);
+           let sortedTitle =  likeArr.map(item => item.listTitle);
+           let sortedLike =  likeArr.map(item => item.likeBtn);
+   
+          setlistTitle(sortedTitle);
+          setlikeBtn(sortedLike);
+         }}
+         ><p>추천순</p></div>
+         <div><p>댓글순</p></div>
+         <div><p>조회순</p></div>
+         </div>
+         </>
+  )
+}
+
+
 function App() {
   let post = '리액트 스터디';
   let [listTitle,setlistTitle] = useState(['ㄷ액트는 왜 쓸까?','ㄴsx는 무엇일까?', 'ㄱtate는 언제쓸까?']);
@@ -19,6 +77,7 @@ function App() {
   let [modal,setModal] = useState(false);
   let [title, setTitle] = useState(0);
   let [value,setValue] = useState('');
+  let [arrBtn, setArrBtn] = useState(false);
 
   return (
     <div className="App">
@@ -32,59 +91,15 @@ function App() {
     <div className='btn-container'>
     <button className="write-Btn">🖌 작성하기</button>&nbsp;&nbsp;&nbsp;
    
-    {/* 정렬종류 select 버튼 */}
-    <button className="arr-Btn"onClick={() => {}}
-   >📌 정렬하기</button>
-</div>
+   {/*  버튼을 누르면 select버튼 나타났다 사라짐*/}
+    <button className="arr-Btn"onClick={() => {setArrBtn(!arrBtn);}}
+      >📌 정렬하기</button>
+   </div>
 
-      <div className='array-select-btn'>
-
-      {/* 오름차순으로 정렬하는 버튼 */}
-      <div onClick={() => {
-       const data = listTitle.map((a, i) => {
-          return {'listTitle' : listTitle[i], 'likeBtn': likeBtn[i] }
-        });
-      
-        let ascArr = data.sort((a,b)=> a.listTitle.localeCompare(b.listTitle));
-        let sortedTitle =  ascArr.map(item => item.listTitle);
-        let sortedLike =  ascArr.map(item => item.likeBtn);
-        setlistTitle(sortedTitle);
-        setlikeBtn(sortedLike);
-      }}>
-      <p>오름차순</p></div>
-      
-      
-      {/* 내림차순으로 정렬하는 버튼 */}
-      <div onClick={()=> {
-      const data = listTitle.map((a, i) => {
-        return {'listTitle' : listTitle[i], 'likeBtn': likeBtn[i] }
-      });
-
-      let descArr = data.sort((a,b)=> a.listTitle.localeCompare(b.listTitle)).reverse();
-      let sortedTitle =  descArr.map(item => item.listTitle);
-      let sortedLike =  descArr.map(item => item.likeBtn);
-      setlistTitle(sortedTitle);
-      setlikeBtn(sortedLike);
-      }}><p>내림차순</p></div>
-
-      {/* 좋아요순으로 정렬하는 버튼 */}
-      <div onClick={()=> {
-      
-      const data = listTitle.map((a , i) => {
-        return { 'listTitle':  listTitle[i], 'likeBtn': likeBtn[i] };
-      });
-
-        let likeArr = data.sort((a,b)=> b.likeBtn- a.likeBtn);
-        let sortedTitle =  likeArr.map(item => item.listTitle);
-        let sortedLike =  likeArr.map(item => item.likeBtn);
-
-       setlistTitle(sortedTitle);
-       setlikeBtn(sortedLike);
-      }}
-      ><p>추천순</p></div>
-      <div><p>댓글순</p></div>
-      <div><p>조회순</p></div>
-      </div>
+  {/* 정렬 select 버튼 */}
+{
+    arrBtn == true? <ArrayBtn listTitle={listTitle} likeBtn={likeBtn} setlistTitle={setlistTitle} setlikeBtn={setlikeBtn}/> : null
+}
 
 {
       listTitle.map((a, i)=>{
